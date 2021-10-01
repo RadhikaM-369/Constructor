ad410b8a928a9c72a000d7efa4091d36f5424c2e
RadhikaM-369 <90022111+RadhikaM-369@users.noreply.github.com>
2021-10-01 10:21:35 +0530

    Add files via upload

---
+++ PhonePayTest.java
@@ -0,0 +1,10 @@
+package com.xworkz.OOPconcepts;
+
+public class PhonePayTest {
+
+	public static void main(String args[]) {
+		//String userName,String place,long PhoneNo,long adharNo,String PAN
+		PhonePay reg= new PhonePay("Radhika","Belgaum",9986408439L,223345645678L,"BC546MH");
+		reg.Registration();
+	}
+}
